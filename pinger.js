c8ca5c78b380d9bdc22339f0d9909099dcb8351e
kustbots <156554454+kustbots@users.noreply.github.com>
2025-08-14 20:00:13 +0530

    Update pinger.js

--- pinger.js
+++ pinger.js
@@ -1,8 +1,8 @@
 export default {
   async scheduled(event, env, ctx) {
     const urls = Object.keys(env)
-      .filter(key => /^\d+$/.test(key))  
-      .map(key => env[key]);             
+      .filter(key => /^\d+$/.test(key)) 
+      .map(key => env[key]);
 
     if (urls.length === 0) {
       console.log('No URLs configured to ping');
@@ -19,5 +19,25 @@ export default {
     });
 
     await Promise.all(pingPromises);
+  },
+
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/") {
+      const urls = Object.keys(env)
+        .filter(key => /^\d+$/.test(key))
+        .map(key => env[key]);
+
+      return new Response(
+        urls.length > 0
+          ? `Currently configured URLs to ping:\n\n${urls.join("\n")}`
+          : "No URLs configured to ping",
+        { status: 200, headers: { "Content-Type": "text/plain" } }
+      );
+    }
+
+    return new Response("Not Found", { status: 404 });
   }
 };
+
